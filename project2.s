65ee4c0c2c8447a41bb575652bc11f0e0b462952
David <davidcharlesdaniels@gmail.com>
2019-11-13 18:51:01 -0500

    Made another variable for the output statement

--- project2.s
+++ project2.s
@@ -2,8 +2,10 @@
 #02884990%11 = 9
 #26 + 9 = Base 35
 #Range (0,y) and (0-Y)
+
 .data 						#Declarations
 InputVariable: .space 1000			#Variable for user input 
+BadInput: .asciiz "Invalid input"		#Variable used to output Invalid input
 .text						#Instructions stored in text segment at next available address
 .globl main					#Allows main to be refrenced anywhere
 
@@ -17,6 +19,7 @@ main:
 	li $t2,4				#Stored to check if variable is greater than 4
 	li $t3,32				#Stored a space in $t3 to check for spaces
 	li $t4,9				#Stored to check for tabs
+	
 	
 	
 	
